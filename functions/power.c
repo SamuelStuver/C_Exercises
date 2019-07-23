33cc5ddfd4e40a6e80166af2ae29f9f864adeef2
Sam Stuver <samuelstuver@gmail.com>
2019-07-23 12:52:31 -0600

    add power function example

---
+++ functions/power.c
@@ -0,0 +1,28 @@
+/* Create and test a function that takes an integer m and raises it to the nth power */
+
+#include <stdio.h>
+
+int power(int m, int n);
+
+main()
+{
+    int i;
+
+    for (i = 0; i < 10; ++i) {
+        printf("%d %d %d\n", i, power(2,i), power(-3,i));
+    }
+    return 0;
+}
+
+/* power: raise base to nth power; n >= 0 */
+int power(int base, int n) {
+
+    int i, p;
+
+    p = 1;
+    for ( i = 1; i <= n; ++i ) {
+        p = p * base;
+    }
+    return p;
+
+}
