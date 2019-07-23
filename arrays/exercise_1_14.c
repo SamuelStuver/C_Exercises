11c018b36a1496306f5d1575f0297d664dcd0875
Sam Stuver <samuelstuver@gmail.com>
2019-07-23 12:14:39 -0600

    add exercise to print histogram of characters in input

--- arrays/exercise_1_14.c
+++ arrays/exercise_1_14.c
@@ -1,1 +1,27 @@
 /* Write a program to print a histogram of the frequencies of different characters in its input */
+#include <stdio.h>
+
+main() {
+
+    int c, i, j, index;
+    int char_frequencies[122 - 32];
+
+    for ( i = 0; i < 90; ++i ) {
+        char_frequencies[i] = 0;
+    }
+
+
+    while ((c = getchar()) != EOF) {
+        index = c - 32;
+        ++char_frequencies[index];
+        //printf("%c -> ", c);
+        //printf("%d\n", c);
+    }
+    for ( i = 0; i < 90; i++ ){
+        printf("%c: ", i+32);
+        for ( j = 0; j < char_frequencies[i]; j++ ){
+            printf("%c", '=');
+        }
+        printf("%c", '\n');
+    }
+}
