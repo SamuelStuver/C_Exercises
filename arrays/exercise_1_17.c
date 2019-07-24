478c67f2afaf195500c970521db1f5a73471d851
Sam Stuver <samuelstuver@gmail.com>
2019-07-23 21:23:44 -0600

    exercise to print all lines greater than X characters

---
+++ arrays/exercise_1_17.c
@@ -0,0 +1,51 @@
+/* Write a program that prints all lines that are longer than 20 characters */
+
+#include <stdio.h>
+#define MAXLINE 1000
+#define MINLINE 20
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
+main()
+{
+    int len; /* current line length */
+    int max; /* maximum length seen so far */
+    char line[MAXLINE]; /* current input line */
+    char current[MAXLINE]; /* longest line saved here */
+
+    max = 0;
+    while ((len = getline(line, MAXLINE)) > 0) {
+        if (len > MINLINE) {
+            printf("%s", line);
+        }
+    }
+    return 0;
+}
+
+/* getline: read a line into s, return length */
+int getline(char s[], int lim)
+{
+    int c, i;
+
+    for (i=0; i<lim-1 && (c = getchar()) != EOF && c!='\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0') {
+        ++i;
+    }
+}
