4e7a348097a28f0df21fb839ac209f8815dabf34
Sam Stuver <samuelstuver@gmail.com>
2019-07-24 11:35:23 -0600

    completed exercise; I think there might be a slight bug?

--- exercises_final_ch_1/exercise_1_21.c
+++ exercises_final_ch_1/exercise_1_21.c
@@ -2,3 +2,31 @@
    and blanks to achieve the same spacing. Use the same tab stops as for detab (1-20)
    When either a tab or a single blank would suffice to reach a tab stop, which should
    be given preference? */
+
+#include <stdio.h>
+#define SPACES_PER_TAB 3
+
+main()
+{
+    /* read a character */
+    int c, blank_count;
+
+    blank_count = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            ++blank_count;
+        } else if (c == '\t') {
+            blank_count += 3;
+        } else {
+            while (blank_count >= SPACES_PER_TAB) {
+                putchar('\t');
+                blank_count -= 3;
+            }
+            while (blank_count > 0) {
+                putchar(' ');
+                --blank_count;
+            }
+            putchar(c);
+        }
+    }
+}
