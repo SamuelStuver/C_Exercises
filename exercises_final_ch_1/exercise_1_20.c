55744b1628fb745af70c2f4b2ef86c0ce1c95c72
Sam Stuver <samuelstuver@gmail.com>
2019-07-24 11:08:16 -0600

    completed exercise 1-20

--- exercises_final_ch_1/exercise_1_20.c
+++ exercises_final_ch_1/exercise_1_20.c
@@ -1,3 +1,22 @@
 /* Write a program detab that replaces tabs in the input with the proper number of blanks
    to space to the next tab stop. Assume a fixed set of tab stops, say every n columns.
    Should n be a variable or a symbolic constant? */
+
+#include <stdio.h>
+#define SPACES_PER_TAB 3
+
+main()
+{
+    /* read a character */
+    int c, i;
+    /* while character is not end-of-file, output character just read, then read a new character */
+    while ((c = getchar()) != EOF){
+        if (c == '\t'){
+            for ( i = 0; i < SPACES_PER_TAB; ++i ) {
+                putchar(' ');
+            }
+        } else {
+            putchar(c);
+        }
+    }
+}
