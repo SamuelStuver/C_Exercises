b14db033daf1370f155a4e96307bab25929c4bfb
Sam Stuver <samuelstuver@gmail.com>
2019-07-24 09:20:27 -0600

    exercise to remove trailing blanks/tabs and remove empty lines

---
+++ arrays/exercise_1_18.c
@@ -0,0 +1,50 @@
+/* Write a program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines */
+
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
+main()
+{
+    int len; /* current line length */
+    char line[MAXLINE]; /* current input line */
+
+    while ((len = getline(line, MAXLINE)) > 0) {
+        //printf("%s", line);
+        while ( line[len] == ' ' || line[len] == '\t' ) {
+                --len;
+        }
+        line[len] = '\0';
+        printf("%s", line);
+    }
+    return 0;
+}
+
+/* getline: read a line into s, return length */
+int getline(char s[], int lim)
+{
+    int c, i;
+
+    for (i=0; i<lim-1 && (c = getchar()) != EOF && c!='\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[])
+{
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0') {
+        ++i;
+    }
+}
