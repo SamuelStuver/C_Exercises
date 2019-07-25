90cd255c2f020c8df1a9ea61b08bc6f4bbf2bc78
Sam Stuver <samuelstuver@gmail.com>
2019-07-25 09:44:42 -0600

    remove quotes from c file exercise

--- exercises_final_ch_1/exercise_1_23.c
+++ exercises_final_ch_1/exercise_1_23.c
@@ -4,29 +4,49 @@
 
 #include <stdio.h>
 
+int toggle(int flag);
 
 int main()
 {
     /* read a character */
     int c_current, c_prev, c_next;
     int inside_comment = 0;
+    int inside_quote = 0;
+
     c_current = getchar();
+    c_next = getchar();
     /* while character is not end-of-file, output character just read, then read a new character */
     while (c_current != EOF){
 
-        c_next = getchar();
+        if ( c_current == '"' ) {
+            inside_quote = toggle(inside_quote);
+        }
+
+
         if ((c_current == '/' && c_next == '*') || (c_prev == '/' && c_current == '*')) {
             inside_comment = 1;
         } else if (c_prev == '*' && c_current == '/') {
             inside_comment = 0;
-            //putchar(c_next);
-        } else if (inside_comment == 0) {
-            putchar(c_next);
+        } else if ((inside_comment == 0 && inside_quote == 0) || (inside_comment == 1 && inside_quote == 1)) {
+            putchar(c_current);
+            //printf("%d-", c_current);
         }
-
 
 
         c_prev = c_current;
         c_current = c_next;
+        c_next = getchar();
+    }
+    putchar('\n');
+}
+
+int toggle(int flag)
+{
+    if (flag == 0) {
+        return 1;
+    } else if (flag == 1) {
+        return 0;
+    } else {
+        return flag;
     }
 }
