085fd8347cab3c2dc8778c86829c1951b8214c44
Sam Stuver <samuelstuver@gmail.com>
2019-07-24 22:04:13 -0600

    almost there, still prints single slash

--- exercises_final_ch_1/exercise_1_23.c
+++ exercises_final_ch_1/exercise_1_23.c
@@ -1,3 +1,32 @@
 /* Write a program to remove all comments from a C program. Don't forget to
    handle quoted strings and character constants properly. C comments do not
    nest. */
+
+#include <stdio.h>
+
+
+int main()
+{
+    /* read a character */
+    int c_current, c_prev, c_next;
+    int inside_comment = 0;
+    c_current = getchar();
+    /* while character is not end-of-file, output character just read, then read a new character */
+    while (c_current != EOF){
+
+        c_next = getchar();
+        if ((c_current == '/' && c_next == '*') || (c_prev == '/' && c_current == '*')) {
+            inside_comment = 1;
+        } else if (c_prev == '*' && c_current == '/') {
+            inside_comment = 0;
+            //putchar(c_next);
+        } else if (inside_comment == 0) {
+            putchar(c_next);
+        }
+
+
+
+        c_prev = c_current;
+        c_current = c_next;
+    }
+}
