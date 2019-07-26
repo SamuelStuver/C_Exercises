a4608367e10c22a92db71c11590562c3b852a9bc
Sam Stuver <samuelstuver@gmail.com>
2019-07-26 13:17:58 -0600

    string to escape char converter, need to do reverse still

---
+++ ch_3/exercise_3_2.c
@@ -0,0 +1,72 @@
+/*
+Write a function escape(s, t) that converts characters like newline and tab
+into visible escape sequences like \n and \t as it copies the string t to s.
+Use a switch.
+>>>DONE
+
+Write a function for the other direction as well, converting
+escape sequences into the real characters.
+*/
+
+void stoescape(char s[], char t[]);
+int getln(char s[], int lim);
+
+#include <stdio.h>
+
+int main() /* count digits, whitespace, others */
+{
+    int len;
+    char t[20];
+    while ((len = getln(t, 20)) > 0) {
+        char s[20];
+        stoescape(s, t);
+        printf("%s\n", s);
+    }
+
+    return 0;
+}
+
+/* copy string t into string s, replacing escape chars with visible escape sequences */
+void stoescape(char s[], char t[])
+{
+    int i, j;
+    for (i = j = 0; t[i] != EOF; i++) {
+        switch (t[i]) {
+            case '\n': {
+                s[j] = '\\';
+                s[++j] = 'n';
+                j++;
+                break;
+                break;
+            }
+            case '\t': {
+                s[j] = '\\';
+                s[++j] = 't';
+                j++;
+                break;
+            }
+            default: {
+                s[j++] = t[i];
+                break;
+            }
+        }
+
+    }
+
+}
+
+/* getln: read a line into s, return length */
+int getln(char s[], int lim)
+{
+    int c, i;
+
+    for (i=0; i<lim-1 && (c = getchar()) != EOF && c!='\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
